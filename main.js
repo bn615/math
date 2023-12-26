a7e85134a8fd4097cb7dc256b12f488d25a654bf
bn615 <chessanole@gmail.com>
2023-12-26 17:32:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,145 +1,189 @@
-const canvasWidth = 800;
-const canvasHeight = 600;
+const canvasWidth = 1000;
+const canvasHeight = 1000;
 
 let vectorLengths = [];
 let rotationSpeeds = [];
 let points = [];
+
 let currentFrame = 0;
 
+
 function setup() {
-  const canvas = createCanvas(canvasWidth, canvasHeight);
-  frameRate(20);
-  canvas.parent('canvas-container');
-  positionUserInput();
+    const canvas = createCanvas(canvasWidth, canvasHeight);
+    frameRate(20);
+    canvas.parent('canvas-container');
+    positionUserInput(); // Call the function to position the user input
+
 }
+
 
 function positionUserInput() {
   const userInputContainer = document.getElementById('user-input-container');
-  userInputContainer.style.display = 'flex';
-  userInputContainer.style.gap = '20px';
+  userInputContainer.style.display = 'flex'; // Use flexbox to arrange columns horizontally
+  userInputContainer.style.gap = '20px'; // Adjust the gap between columns as needed
 
+  // Position the user input form
   const userForm = document.querySelector('.user-input-form');
-  userForm.style.flex = '1';
-  userForm.style.marginRight = '20px';
+  userForm.style.flex = '1'; // Adjust flex property as needed
+  userForm.style.marginRight = '20px'; // Add margin to the right to create space
 
-  const formX = window.innerWidth / 4 + 1000;
-  const formY = 100;
+  const formX = window.innerWidth / 4 + 1000; // X position for the user input form (centered horizontally)
+  const formY = 100; // Y position for the user input above the coordinates header
   userForm.style.position = 'absolute';
   userForm.style.left = formX + 'px';
   userForm.style.top = formY + 'px';
 }
 
+
 function addValue() {
   const inputValue = document.getElementById('userInput').value;
   if (inputValue !== '') {
+    // Add vector length to the array
     vectorLengths.push(Number(inputValue));
+
+    // Add a new row to the user input table
     const tableBody = document.getElementById('user-input-table');
     const newRow = tableBody.insertRow();
+
+    // Add cells to the new row
     const vectorLengthCell = newRow.insertCell(0);
     const rotationSpeedCell = newRow.insertCell(1);
+
+    // Set the text content of the cells
     vectorLengthCell.textContent = inputValue;
-    rotationSpeedCell.textContent = '';
+    rotationSpeedCell.textContent = ''; // Leave rotation speed cell empty
+
+    // Clear the input value
     document.getElementById('userInput').value = '';
-    setDeleteButton(newRow);
   }
 }
 
 function addUserInput() {
-  const vectorLengthInput = document.getElementById('vectorLength').value;
-  const rotationSpeedInput = document.getElementById('rotationSpeed').value;
-
-  if (vectorLengthInput !== '' && rotationSpeedInput !== '') {
-    vectorLengths.push(Number(vectorLengthInput));
-    rotationSpeeds.push(Number(rotationSpeedInput));
-    const tableBody = document.getElementById('user-input-table');
-    const newRow = tableBody.insertRow();
-    const vectorLengthCell = newRow.insertCell(0);
-    const rotationSpeedCell = newRow.insertCell(1);
-    vectorLengthCell.textContent = vectorLengthInput;
-    rotationSpeedCell.textContent = rotationSpeedInput;
-    document.getElementById('vectorLength').value = '';
-    document.getElementById('rotationSpeed').value = '';
-    setDeleteButton(newRow);
-  }
-}
-
-function setDeleteButton(row) {
-  const deleteButton = document.createElement('button');
-  deleteButton.textContent = 'Delete';
-  deleteButton.onclick = function() {
-    deleteUserInput(row);
-  };
-
-  const deleteCell = row.insertCell(2);
-  deleteCell.appendChild(deleteButton);
+    const vectorLengthInput = document.getElementById('vectorLength').value;
+    const rotationSpeedInput = document.getElementById('rotationSpeed').value;
+  
+    if (vectorLengthInput !== '' && rotationSpeedInput !== '') {
+      // Add vector length and rotation speed to their respective arrays
+      vectorLengths.push(Number(vectorLengthInput));
+      rotationSpeeds.push(Number(rotationSpeedInput));
+  
+      // Add a new row to the user input table
+      const tableBody = document.getElementById('user-input-table');
+      const newRow = tableBody.insertRow();
+  
+      // Add cells to the new row
+      const vectorLengthCell = newRow.insertCell(0);
+      const rotationSpeedCell = newRow.insertCell(1);
+  
+      // Set the text content of the cells
+      vectorLengthCell.textContent = vectorLengthInput;
+      rotationSpeedCell.textContent = rotationSpeedInput;
+  
+      // Add a button to delete the row
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.onclick = function() {
+        // Get the index of the row and delete it
+        const rowIndex = newRow.rowIndex; // Subtract 1 to account for the header row
+        deleteUserInput(rowIndex);
+      };
+  
+      // Add the delete button to the row
+      const deleteCell = newRow.insertCell(2);
+      deleteCell.appendChild(deleteButton);
+  
+      // Clear the input values
+      document.getElementById('vectorLength').value = '';
+      document.getElementById('rotationSpeed').value = '';
+    }
 }
 
 function addRotationSpeed() {
-  const rotationSpeedValue = document.getElementById('rotationSpeed').value;
-  const tableBody = document.getElementById('user-input-table');
-  const lastRow = tableBody.lastChild;
-
-  if (rotationSpeedValue !== '' && lastRow) {
-    const rotationSpeedCell = lastRow.lastChild;
-    const rotationSpeedInput = document.createElement('input');
-    rotationSpeedInput.type = 'number';
-    rotationSpeedInput.value = rotationSpeedValue;
-    rotationSpeedInput.disabled = true;
-    rotationSpeedCell.appendChild(rotationSpeedInput);
-    rotationSpeeds.push(Number(rotationSpeedValue));
-  }
+    const rotationSpeedValue = document.getElementById('rotationSpeed').value;
+    const tableBody = document.getElementById('user-input-table');
+    const lastRow = tableBody.lastChild;
+  
+    if (rotationSpeedValue !== '' && lastRow) {
+      // Find the rotation speed cell in the last row
+      const rotationSpeedCell = lastRow.lastChild;
+  
+      // Create a new input element for rotation speed
+      const rotationSpeedInput = document.createElement('input');
+      rotationSpeedInput.type = 'number';
+      rotationSpeedInput.value = rotationSpeedValue;
+      rotationSpeedInput.disabled = true;
+  
+      // Append the new input element to the rotation speed cell
+      rotationSpeedCell.appendChild(rotationSpeedInput);
+  
+      // Add the new rotation speed to the array
+      rotationSpeeds.push(Number(rotationSpeedValue));
+    }
 }
 
-function deleteUserInput(row) {
-  const rowIndex = row.rowIndex - 1;
-  row.remove();
-  vectorLengths.splice(rowIndex, 1);
-  rotationSpeeds.splice(rowIndex, 1);
+
+function deleteUserInput(index) {
+    // Remove the selected row from the table
+    const tableBody = document.getElementById('user-input-table');
+    tableBody.deleteRow(index);
+  
+    // Remove the corresponding values from the arrays
+    vectorLengths.splice(index, 1);
+    rotationSpeeds.splice(index, 1);
 }
+
+
 
 function draw() {
-  background(255);
-  drawGrid(20, 20);
+    background(255);
+  
+    drawGrid(20, 20); // Adjust the grid spacing as needed
+        
 
-  let x = width / 2;
-  let y = height / 2;
+    let x = width / 2;
+    let y = height / 2;
 
-  fill(0);
-  noStroke();
-  ellipse(width / 2, height / 2, 10, 10);
 
-  for (let i = 0; i < vectorLengths.length; i++) {
-    let prevX = x;
-    let prevY = y;
-
-    let n = i * 2 + 1;
-
-    let radius = vectorLengths[i];
-    let speed = radians(rotationSpeeds[i]);
-
-    x += radius * cos(n * currentFrame * speed);
-    y += radius * sin(n * currentFrame * speed);
-
-    stroke(0);
-    line(prevX, prevY, x, y);
-    ellipse(prevX, prevY, 5, 5);
+    fill(0); // Set the fill color to black
+    noStroke(); // Disable stroke (outline)
+    ellipse(width / 2, height / 2, 10, 10); // Draw an ellipse at the center
+  
+    for (let i = 0; i < vectorLengths.length; i++) {
+      let prevX = x;
+      let prevY = y;
+  
+      let n = i * 2 + 1; // Adjust this factor for more or fewer rotations
+  
+      let radius = vectorLengths[i];
+      let speed = radians(rotationSpeeds[i]);
+  
+      x += radius * cos(n * currentFrame * speed);
+      y += radius * sin(n * currentFrame * speed);
+  
+      // Draw the vector
+      stroke(0);
+      line(prevX, prevY, x, y);
+      ellipse(prevX, prevY, 5, 5);
+    }
+  
+    // Draw the final point
+    fill(0);
+    ellipse(x, y, 10, 10);
+  
+    currentFrame++;
   }
 
-  fill(0);
-  ellipse(x, y, 10, 10);
-
-  currentFrame++;
-}
-
-function drawGrid(spacingX, spacingY) {
-  stroke(200);
-  strokeWeight(1);
-
-  for (let x = 0; x <= width; x += spacingX) {
-    line(x, 0, x, height);
+  function drawGrid(spacingX, spacingY) {
+    stroke(200); // Set the stroke color to light gray
+    strokeWeight(1); // Set the stroke weight to 1
+  
+    // Draw vertical lines
+    for (let x = 0; x <= width; x += spacingX) {
+      line(x, 0, x, height);
+    }
+  
+    // Draw horizontal lines
+    for (let y = 0; y <= height; y += spacingY) {
+      line(0, y, width, y);
+    }
   }
-
-  for (let y = 0; y <= height; y += spacingY) {
-    line(0, y, width, y);
-  }
-}
