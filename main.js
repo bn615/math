dadc5cf18a736c46afde3ce8fc46cd165d7dbbaa
bn615 <chessanole@gmail.com>
2024-01-01 09:42:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -233,7 +233,7 @@ function draw() {
 
   // Highlight the path traced out by the endpoint of the last vector
   stroke(0, 255, 0);
-  for (let i = 2; i < endPointPath.length; i++) {
+  for (let i = 3; i < endPointPath.length; i++) {
     line(endPointPath[i - 1].x, endPointPath[i - 1].y, endPointPath[i].x, endPointPath[i].y);
   }
 
